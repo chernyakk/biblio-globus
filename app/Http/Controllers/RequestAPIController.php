f033c1f3859f7dd63d41a45db84eb94c3c0dd427
Artyom Chernyak <chernyak.artyom@gmail.com>
2020-07-27 08:54:53 +0000

    Changed hotel on other

--- app/Http/Controllers/RequestAPIController.php
+++ app/Http/Controllers/RequestAPIController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\APIRequest;
 use App\ExcelFile;
-use DateTime;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
